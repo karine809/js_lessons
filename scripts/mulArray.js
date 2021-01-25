f44798871ae6ac78ee3da4697d4cf5c755a157fb
karine809 <karinemesropyan1984@gmail.com>
2021-01-25 12:59:49 +0400

    MultyArray

---
+++ scripts/mulArray.js
@@ -0,0 +1,19 @@
+var arr = [
+    ["wake up", 8],
+    ["breakfast", 9],
+    ["work", 12],
+    ["lunch", 14],
+    ["sleep", 22]
+];
+arr.push(["homework", 16]);
+arr.pop();
+arr.splice(4,0,["program lesson", 19])
+ console.table(arr);
+// console.log(arr[4][0]);
+for (var i =0 ; i < arr.length; i++){
+    //console.log(arr[i]);
+    var innerArraylength = arr[i].length;
+    for(var j = 0; j < innerArraylength; j++){
+        console.log('[' + i + ',' + j + ']= ' + arr[i][j]);
+    }
+}
