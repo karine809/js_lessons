2a03eff943d511468b0e05227350c82869182f83
karine809 <karinemesropyan1984@gmail.com>
2021-02-05 15:53:40 +0400

    for in/ for of loops

---
+++ scripts/loops.js
@@ -0,0 +1,14 @@
+//for of loop
+const array = [3, 6, 40, 71];
+//returns value
+for (let i of array){//veradardznum e valuen
+    console.log(i);
+}
+ const obj = {
+      name: "Karine",
+      age: "33"
+ }
+//returns key
+for (let i  in obj){//veradardznum e index@
+    console.log(i);
+}
